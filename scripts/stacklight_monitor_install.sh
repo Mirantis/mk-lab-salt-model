394c3dbd87195161dcd13343156fbdd265a22341
Simon Pasquier <spasquier@mirantis.com>
2016-11-25 11:54:08 +0100

    Update install scripts for StackLight services
    This change runs the client counterparts for Grafana and Kibana.

--- scripts/stacklight_monitor_install.sh
+++ scripts/stacklight_monitor_install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash -x
 
 # StackLight services
-salt -C 'I@elasticsearch:server' state.sls elasticsearch -b 1
+salt -C 'I@elasticsearch:server' state.sls elasticsearch.server -b 1
 salt -C 'I@influxdb:server' state.sls influxdb -b 1
-salt -C 'I@kibana:server' state.sls kibana
-salt -C 'I@grafana:server' state.sls grafana
+salt -C 'I@kibana:server' state.sls kibana.server
+salt -C 'I@grafana:server' state.sls grafana.server -b 1
+
+# Gather Grafana dashboards from all nodes
+salt -C 'I@grafana:collector' state.sls grafana.collector
+salt "*" state.sls salt.minion.grains
+salt "*" mine.flush
+salt "*" saltutil.refresh_modules
+salt "*" mine.update
+
+sleep 5
+
+salt -C 'I@kibana:client' state.sls kibana.client
+salt -C 'I@grafana:client' state.sls grafana.client
