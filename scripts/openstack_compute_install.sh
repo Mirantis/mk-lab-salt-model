9e7a441814073a3ad53a7d5d8ca377f86e90cdbe
Simon Pasquier <spasquier@mirantis.com>
2016-11-30 15:17:26 +0100

    Provision OpenContrail services

--- scripts/openstack_compute_install.sh
+++ scripts/openstack_compute_install.sh
@@ -5,6 +5,9 @@ exec > >(tee -i /tmp/$(basename $0 .sh)_$(date '+%Y-%m-%d_%H-%M-%S').log) 2>&1
 salt "cmp*" state.apply
 salt "cmp*" state.apply
 
+# Provision opencontrail virtual routers
+salt -C 'I@opencontrail:control:id:1' cmd.run "/usr/share/contrail-utils/provision_vrouter.py --host_name cmp01 --host_ip 172.16.10.105 --api_server_ip 172.16.10.254 --oper add --admin_user admin --admin_password workshop --admin_tenant_name admin"
+
 # Reboot compute nodes
 salt "cmp*" system.reboot
 
