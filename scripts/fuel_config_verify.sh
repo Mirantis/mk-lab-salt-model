fbd228debffc163fc2af72f054b68088e125ab36
Olivier Bourdon <obourdon@mirantis.com>
2016-12-13 11:22:08 +0100

    Add safety loop
    This prevents errors when lauching deployment scritps
    too early (aka when all nodes are not yet bootstrapped
    and answering to salt pings)

--- scripts/fuel_config_verify.sh
+++ scripts/fuel_config_verify.sh
@@ -8,3 +8,25 @@ reclass-salt --top
 # Verify that Salt minions are responding and the same version as master
 salt-call --version
 salt '*' test.version
+
+# Get list of hostnames in current deployment
+hosts=(`salt-call pillar.get linux:network:host | egrep '0.*:' | sed -e 's/  *//' -e 's/://'`)
+wanted=${#hosts[@]}
+
+# Default max waiting time is 5mn
+MAX_WAIT=${MAX_WAIT:-300}
+while [ true ]; do
+	nb_nodes=`salt '*' test.ping | egrep ':$' | wc -l`
+	if [ -n "$nb_nodes" ] && [ $nb_nodes -eq $wanted ]; then
+		echo "All nodes are now answering to salt pings"
+		break
+	fi
+	MAX_WAIT=`expr $MAX_WAIT - 15`
+	if [ $MAX_WAIT -le 0 ]; then
+		echo "Only $nb_nodes answering to salt pings out of $wanted after maximum timeout"
+		exit 1
+	fi
+	echo -n "Only $nb_nodes answering to salt pings out of $wanted. Waiting a bit longer ..."
+	sleep 15
+	echo
+done
