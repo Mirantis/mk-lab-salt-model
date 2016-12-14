03764b0b77a601b6fca4f7e276231b93443047a2
Olivier Bourdon <obourdon@mirantis.com>
2016-12-14 11:57:36 +0100

    Add code factorization

--- scripts/fuel_config_verify.sh
+++ scripts/fuel_config_verify.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
 exec > >(tee -i /tmp/$(basename $0 .sh)_$(date '+%Y-%m-%d_%H-%M-%S').log) 2>&1
 
+# Import common functions
+COMMONS=./common_functions.sh
+if [ ! -f $COMMONS ]; then
+	echo "File $COMMONS does not exist"
+	exit 1
+fi
+. $COMMONS
+
 # Verify that Salt master is correctly bootstrapped
 salt-key
 reclass-salt --top
@@ -9,23 +17,5 @@ reclass-salt --top
 salt-call --version
 salt '*' test.version
 
-# Get number of hostnames in current deployment
-wanted=$(salt-call pillar.get linux:network:host --out key | sed 's/:.*//' | grep '[0-9]' | wc -l)
-
-# Default max waiting time is 5mn
-MAX_WAIT=${MAX_WAIT:-300}
-while [ true ]; do
-	nb_nodes=$(salt '*' test.ping --out txt | grep True | wc -l)
-	if [ -n "$nb_nodes" ] && [ $nb_nodes -eq $wanted ]; then
-		echo "All nodes are now answering to salt pings"
-		break
-	fi
-	MAX_WAIT=$(( $MAX_WAIT - 15 ))
-	if [ $MAX_WAIT -le 0 ]; then
-		echo "Only $nb_nodes answering to salt pings out of $wanted after maximum timeout"
-		exit 1
-	fi
-	echo -n "Only $nb_nodes answering to salt pings out of $wanted. Waiting a bit longer ..."
-	sleep 15
-	echo
-done
+# Wait for all nodes in current deployment to be available
+wait_for $(salt-call pillar.get linux:network:host --out key | sed 's/:.*//' | grep '[0-9]' | wc -l)
