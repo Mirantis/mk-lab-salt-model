ec86e8da5a9f8605028c0a2b7d7cbfa2cb4625c2
Olivier Bourdon <obourdon@mirantis.com>
2016-12-14 12:09:09 +0100

    Use factorized code

--- scripts/openstack_compute_install.sh
+++ scripts/openstack_compute_install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
 exec > >(tee -i /tmp/$(basename $0 .sh)_$(date '+%Y-%m-%d_%H-%M-%S').log) 2>&1
 
+# Import common functions
+COMMONS=./common_functions.sh
+if [ ! -f $COMMONS ]; then
+	echo "File $COMMONS does not exist"
+	exit 1
+fi
+. $COMMONS
+
 # Configure compute nodes
 salt "cmp*" state.apply
 salt "cmp*" state.apply
@@ -11,12 +19,5 @@ salt -C 'I@opencontrail:control:id:1' cmd.run "/usr/share/contrail-utils/provisi
 # Reboot compute nodes
 salt "cmp*" system.reboot
 
-while true; do
-	salt '*' test.ping | grep -q Minion
-	if [ $? -ne 0 ]; then
-		break
-	fi
-	echo -n "Waiting for compute nodes to answer salt pings ..."
-	sleep 10
-	echo
-done
+# Wait for all compute nodes in current deployment to be available
+wait_for $(salt-call pillar.get linux:network:host --out key | sed 's/:.*//' | grep 'cmp[0-9]' | wc -l) "cmp*"
