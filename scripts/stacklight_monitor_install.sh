f7b8f4170aef7c7d5d38384fd9e3fac89057a695
Simon Pasquier <spasquier@mirantis.com>
2017-01-06 10:09:38 +0100

    Clean up grains before deploying StackLight agents

--- scripts/stacklight_monitor_install.sh
+++ scripts/stacklight_monitor_install.sh
@@ -2,9 +2,14 @@
 exec > >(tee -i /tmp/"$(basename "$0" .sh)"_"$(date '+%Y-%m-%d_%H-%M-%S')".log) 2>&1
 
 # Start by flusing Salt Mine to make sure it is clean
+# Also clean-up the grains files to make sure that we start from a clean state
 salt "*" mine.flush
+salt "*" file.remove /etc/salt/grains.d/collectd
+salt "*" file.remove /etc/salt/grains.d/heka
+salt "*" file.remove /etc/salt/grains
 
-# Install StackLight services, and gather the Collectd and Heka metadata
+# Install collectd and heka services on the nodes, this will also generate the
+# metadata that goes into the grains and eventually into Salt Mine
 salt "*" state.sls collectd
 salt "*" state.sls heka
 
@@ -21,7 +26,7 @@ sleep 5
 # Update Heka
 salt -C 'I@heka:aggregator:enabled:True or I@heka:remote_collector:enabled:True' state.sls heka
 
-# Update Collectd
+# Update collectd
 salt -C 'I@collectd:remote_client:enabled:True' state.sls collectd
 
 # Update Nagios
@@ -42,7 +47,7 @@ salt -G "ipv4:$vip" service.start remote_collectd
 salt -G "ipv4:$vip" service.start remote_collector
 salt -G "ipv4:$vip" service.start aggregator
 
-# Stop Nagios on monitoring nodes (b/c package starts it by default), then
+# Stop Nagios on monitoring nodes (b/c the package starts it by default), then
 # start Nagios where the VIP is running.
 salt -C 'I@nagios:server:automatic_starting:False' service.stop nagios3
 salt -G "ipv4:$vip" service.start nagios3
